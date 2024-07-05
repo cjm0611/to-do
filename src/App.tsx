67504ac2aeffccaaf392b37ed1cae310f1ef3fc1
cjm0611 <cjm-0611@naver.com>
2024-07-05 11:11:48 +0900

    feat(App): add to-do item by using mui icon

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import TodoItem from './components/ToDoItem';
 import { kadvice } from 'kadvice';
+import AddIcon from '@mui/icons-material/Add';
 
 function App() {
   const currentDate: Date = new Date();
-  const [advice, setAdvice] = useState(kadvice.getOne()); // useState를 이용하여 advice 상태를 관리합니다.
-  const today: string = `${currentDate.getFullYear()}년 ${currentDate.getMonth()}월 ${currentDate.getDay()}일`;
+  const [advice, setAdvice] = useState(kadvice.getOne());
+  const today: string = `${currentDate.getFullYear()}년 ${currentDate.getMonth() + 1}월 ${currentDate.getDay()}일`;
   const [todos, setTodos] = useState([
     { text: 'React useMemo 공부하기', isCompleted: false },
     { text: '구름 코테 가입하기', isCompleted: false },
@@ -35,10 +36,10 @@ function App() {
 
   return (
     <div className="w-full h-screen bg-gradient-to-b from-cyan-800 to-gray-300 p-[100px] flex items-center justify-center font-pre">
-      <div className="min-w-[1000px] max-w-full h-full rounded-[20px] p-[30px] bg-white drop-shadow-xl">
+      <div className="w-[500px] h-full rounded-[20px] p-[50px] bg-white drop-shadow-xl">
         <h1 className="text-3xl text-gray-800 font-semibold">{today}</h1>
         <p className="text-gray-500 italic mt-[10px]">{`${advice.message} - ${advice.author}`}</p>
-        <section className="mt-[30px]">
+        <main className="mt-[30px]">
           <p className="text-xl font-bold mb-[10px]">To Do List ✅</p>
           <ul>
             {todos.map((todo, index) => (
@@ -51,21 +52,19 @@ function App() {
               </li>
             ))}
           </ul>
-        </section>
+          <button className="flex items-center justify-center">
+            <AddIcon style={{ color: '#5C5AD9' }} />
+            <p>할 일 추가</p>
+          </button>
+        </main>
         <section className="mt-[50px] flex items-center">
           <input
             type="text"
-            className="w-[300px] border-b focus:border-blue-500 outline-none"
+            className="w-full border-b focus:border-blue-500 outline-none"
             placeholder="해야 할 일을 입력하세요."
             onChange={(e) => setNewToDoText(e.target.value)}
             value={newToDoText}
           />
-          <button
-            className="w-[50px] h-[25px] ml-[15px] bg-black text-white rounded-[8px]"
-            onClick={handleAddTodo}
-          >
-            추가
-          </button>
         </section>
       </div>
     </div>
